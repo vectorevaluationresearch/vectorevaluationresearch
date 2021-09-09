173200372641278b30b2f7a251f92dd459eb6f0c
daleyli <89942415+vectorevaluationresearch@users.noreply.github.com>
2021-09-08 23:28:05 -0400

    Update

--- README.md
+++ README.md
@@ -1,3 +1,91 @@
-# Toward Hybrid Code Representation Learning Models between Static and Contextualized Embeddings for Bug Detection
+# CDFixCode
 
-### Please see: https://vectorevaluationresearch.github.io/vectorevaluationresearch/ for processed webpage
+This repository contains the code for **Toward Hybrid Code Representation Learning Models between Static and Contextualized Embeddings for Bug Detection** and the [Page](https://vectorevaluationresearch.github.io/vectorevaluationresearch/) that has visualized data.
+
+## Contents
+
+1. [Website](#Website)
+2. [Introduction](#Introduction)
+3. [Dataset](#Dataset)
+4. [Requirement](#Requirement)
+5. [Instruction](#Instruction)
+6. [Demo](#Demo)
+
+## Website
+
+We published the visualized experiment of our reasearch in https://vectorevaluationresearch.github.io/vectorevaluationresearch/
+
+## Introduction
+
+Polysemy in natural language texts is the coexistence of many
+possible meanings for a word or phrase in different contexts. However,
+due to different nature, in source code, some code tokens and
+statements exhibit polysemy while other tokens (e.g., keywords,
+separators, and operators) or statements maintain the same meaning
+in different contexts. Polysemy is crucial in automated bug
+detection because some tokens/statements with the same lexemes
+are buggy in certain contexts, and not in others. Thus, several existing
+bug detection approaches have moved from using static word
+embedding models for code representation learning (CRL) to the
+contextualized models (i.e., the embeddings are sensitive to the surrounding
+contexts). While the intention and design mechanisms of
+contextualized CRL models are to capture the contextual information
+of program units in different representations (code sequences,
+ASTs, graphs), the key questions on the use of static, contextualized,
+or hybrid models remain unanswered due to the nature of mixed
+polysemy in source code as explained.
+We conducted our experiments on a total of 12 popular sequence-
+/tree-/graph-based embedding models and on samples of a dataset of
+10,222 Java projects with +14M methods. We modified and adapted
+four evaluation metrics for contextuality from texts to code structures
+to evaluate the vector representations produced by those CRL
+models.We reported several important findings. For example, different
+CRL models exhibit different levels of contextuality: the models
+with higher contextuality help a bug detection model perform better
+than the static ones. However, the most important finding is
+that a simple hybrid model actually helps a machine-learning bug
+detection model perform better than both static and contextualized
+models. Thus, we call for actions to develop hybrid CRL models
+that fit with the mixed polysemy nature of source code.
+
+## Dataset
+
+### Preprocessed Dataset
+
+We published our processed dataset at 
+
+Please download the dataset, unzip it and put all files in ```./processed``` folder.
+
+### Use your own dataset
+
+If you want to use your own dataset, please prepare the data as follow:
+
+1. There is one data file called ```data.npy```
+
+2. ```data.npy``` includes a list of methods. For each bug, there are four types of information
+
+	1> ```Bug_label```: A ```1*2``` matrix that represent if the method is buggy
+	
+	2> ```Token_label```: A ```N*1``` matrix that represent if the token should be processed by bert. ```N``` is the total token in the method (padded to the max one). 
+	
+	3> ```Bert_embedding```: A ```N*E``` matrix stores all bert embeddings. ```E``` is the embedding size.
+	
+	4> ```Word2vec_embedding```: A ```N*E``` matrix stores all word2vec embeddings. 
+	
+	```data.npy``` stores these info with a list as ```[[Bug_label, Token_label, Bert_embedding, Word2vec_embedding], ... ]```
+
+## Requirement
+
+Please check all required packages in the [requirement.txt]()
+
+## Instruction
+
+Change the ```split``` value to define your data splitting position.
+
+Run ```main.py``` to see the result for the experiment. 
+
+## Demo
+
+For the testing purpose of running, please download our demo that contains a small set of data. Demo download: https://drive.google.com/file/d/1nLea0vpvG4KAyBGy9VTQwXbB53QmH8oP/view?usp=sharing
+
+Put ```processed``` in the same folder of this repo and then run ```main.py``` to see the results.
---
+++ main.py
@@ -0,0 +1,110 @@
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Input, Activation, Flatten
+from tensorflow.keras.regularizers import l2
+import numpy as np
+import os
+from sklearn.metrics import classification_report
+
+
+def svm_model(input_length, input_dim, hidden_dim):
+    input_data = Input(shape=(input_length, input_dim))
+    input_data_f = Flatten()(input_data)
+    hidden = Dense(hidden_dim, activation='relu')(input_data_f)
+    hidden = Dense(2, kernel_regularizer=l2(0.01))(hidden)
+    output = Activation('softmax')(hidden)
+    model = Model(input_data, output)
+    model.compile(loss='hinge', optimizer='adadelta', metrics=['Precision', 'Recall', 'AUC'])
+    return model
+
+
+def model_1(data, split):
+    processed_data = []
+    for i in range(len(data)):
+        processed_data.append(data[i][2])
+    return np.array(processed_data[:split]), np.array(processed_data[split:])
+
+
+def model_2(data, split):
+    processed_data = []
+    for i in range(len(data)):
+        subprocess = []
+        for j in range(len(data[i][1])):
+            label = data[i][1][j]
+            if label == 1:
+                subprocess.append(np.concatenate((data[i][2][j], np.zeros((768,))), axis=0))
+            else:
+                subprocess.append(np.concatenate((np.zeros((768,)), data[i][3][j]), axis=0))
+        processed_data.append(subprocess)
+    return np.array(processed_data[:split]), np.array(processed_data[split:])
+
+
+def model_3(data, split):
+    processed_data = []
+    for i in range(len(data)):
+        subprocess = []
+        for j in range(len(data[i][1])):
+            label = data[i][1][j]
+            if label == 1:
+                subprocess.append(data[i][2][j])
+            else:
+                subprocess.append(data[i][3][j])
+        processed_data.append(subprocess)
+    return np.array(processed_data[:split]), np.array(processed_data[split:])
+
+
+def model_4(data, split):
+    processed_data = []
+    for i in range(len(data)):
+        subprocess = []
+        for j in range(len(data[i][1])):
+            label = data[i][1][j]
+            if label == 1:
+                subprocess.append(data[i][2][j])
+        processed_data.append(subprocess)
+    for i in range(len(data)):
+        max_length = len(data[i][2][0])
+        if len(processed_data[i]) < max_length:
+            if len(processed_data[i]) > 0:
+                processed_data[i] = np.concatenate((processed_data[i], np.zeros((max_length - len(processed_data[i]), 768))), axis=0)
+            else:
+                processed_data[i] = np.zeros((max_length - len(processed_data[i]), 768))
+    return np.array(processed_data[:split]), np.array(processed_data[split:])
+
+
+def get_label_data(data, split):
+    processed_data = []
+    for i in range(len(data)):
+        processed_data.append(data[i][0])
+    return np.array(processed_data[:split][:]).reshape(-1, 2), np.array(processed_data[split:][:]).reshape(-1, 2)
+
+
+if __name__ == '__main__':
+    split = 180
+    input_data = np.load(os.path.join("D:\\research\\interpretable\\vectorevaluationresearch\\processed\\", "data.npy"), allow_pickle=True)
+    label_data, test_label = get_label_data(input_data, split)
+    input_1, test_1 = model_1(input_data, split)
+    input_2, test_2 = model_2(input_data, split)
+    input_3, test_3 = model_3(input_data, split)
+    input_4, test_4 = model_4(input_data, split)
+    m_1 = svm_model(len(input_1[0]), len(input_1[0][0]), 128)
+    m_2 = svm_model(len(input_2[0]), len(input_2[0][0]), 128)
+    m_3 = svm_model(len(input_3[0]), len(input_3[0][0]), 128)
+    m_4 = svm_model(len(input_4[0]), len(input_4[0][0]), 128)
+    m_1.fit(input_1, label_data, batch_size=128, epochs=50)
+    m_2.fit(input_2, label_data, batch_size=128, epochs=50)
+    m_3.fit(input_3, label_data, batch_size=128, epochs=50)
+    m_4.fit(input_4, label_data, batch_size=128, epochs=50)
+    out_1 = m_1.predict(test_1)
+    out_1 = np.argmax(out_1, axis=1)
+    test_label = np.argmax(test_label, axis=1)
+    print(classification_report(test_label, out_1))
+    out_2 = m_2.predict(test_2)
+    out_2 = np.argmax(out_2, axis=1)
+    print(classification_report(test_label, out_2))
+    out_3 = m_3.predict(test_3)
+    out_3 = np.argmax(out_3, axis=1)
+    print(classification_report(test_label, out_3))
+    out_4 = m_4.predict(test_4)
+    out_4 = np.argmax(out_4, axis=1)
+    print(classification_report(test_label, out_4))
+
